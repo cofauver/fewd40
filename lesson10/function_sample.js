431b1273a36d9d1c001289d32754049294fff29f
Cory Fauver <cofauver@gmail.com>
2016-02-18 17:45:13 -0800

    added the simple little funciton sample to the materials

---
+++ lesson10/function_sample.js
@@ -0,0 +1,9 @@
+function rectangleArea(height, width){
+	return height*width;
+}
+
+var rectangle1 = rectangleArea(10,20);
+var rectangle2 = rectangleArea(5,92);
+
+console.log(rectangle1);
+console.log(rectangle2);
